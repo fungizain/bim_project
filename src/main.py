8d206495bd3b81c222f7d5bfe0984ec6276d30de
fungizain <simonngwaifung@gmail.com>
2026-01-16 17:43:54 +0800

    fix spacing

--- src/main.py
+++ src/main.py
@@ -1,15 +1,12 @@
 import os
 import uvicorn
 
-
 def main():
     uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=True)
-
 
 def dev_main():
     os.environ["APP_ENV"] = "dev"
     main()
-
 
 def prod_main():
     os.environ["APP_ENV"] = "prod"
