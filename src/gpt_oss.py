7146cc922f1a060eabf52bf1f66f375f82ae027d
fungizain <simonngwaifung@gmail.com>
2026-01-27 01:01:50 +0800

    fix load model param

--- src/gpt_oss.py
+++ src/gpt_oss.py
@@ -118,7 +118,6 @@ stop_token_ids = encoding.stop_tokens_for_assistant_actions()
 model_id = "openai/gpt-oss-20b"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = GptOssForCausalLM.from_pretrained(
-    "text-generation",
     model=model_id,
     dtype="auto",
     device_map="auto"
